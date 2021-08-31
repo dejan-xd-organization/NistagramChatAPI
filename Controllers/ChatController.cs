d98c8eee3509f4f88afac0912a9e8586df027370
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-31 15:49:50 +0200

    initial work

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NistagramChatAPI.Service;
+using NistagramSQLConnection.Model;
+using NistagramUtils.DTO.Chat;
+
+namespace NistagramChatAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+
+        private readonly IChatApiService _iChatApiService;
+        private readonly IMapper _mapper;
+
+        public ChatController(IChatApiService iChatApiService, IMapper mapper)
+        {
+            _iChatApiService = iChatApiService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("/[action]")]
+        public Object GetChatByUser(long friendId, long userId)
+        {
+            List<Message> messages = _iChatApiService.GetChatByUser(friendId, userId);
+            return JsonConvert.SerializeObject(messages);
+        }
+
+        [HttpPost]
+        [Route("/[action]")]
+        public Object SendMessage(SendMessageDto sendMessageDto)
+        {
+            Message message = _iChatApiService.SendMessage(sendMessageDto);
+            return JsonConvert.SerializeObject(message);
+        }
+    }
+}
